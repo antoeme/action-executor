6cbfeacb3ede4e458976926aeacfaefc4b0395ac
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-08-09 12:23:15 +0200

    inseriti thread e controlli nel polling

--- esecuzione.py
+++ esecuzione.py
@@ -1,21 +1,37 @@
+import re
 import requests
 import time
 
 GET_TEMP = "http://127.0.0.1:5000/temp"
 SET_RELAY_1 = "http://127.0.0.1:5001/set_relay/1"
 SET_RELAY_2 = "http://127.0.0.1:5001/set_relay/2"
+STATUS_RELAY = "http://127.0.0.1:5001/status"
 
+RUN = True
 
+# def controllo_temp():
+#     while(RUN):
+#         temp = requests.get(GET_TEMP).json()
+#         stato_r = requests.get(STATUS_RELAY).json()
+#         print(stato_r)
+#         for chiave in temp:
+#             print(temp[chiave])
+#             if(temp[chiave] >= 20.0 and temp[chiave]<= 30.0 and stato_r["Relay1"] == 0 ): requests.get(SET_RELAY_1)
+#             elif(temp[chiave] >= 40.0 and stato_r["Relay2"] == 0 ): requests.get(SET_RELAY_2)
+#         time.sleep(10)
+    
 def controllo_temp():
-    while(True):
-        response_temps = requests.get(GET_TEMP)
-        temp = response_temps.json()
-        for chiave in temp:
-            print(temp[chiave])
-            if(temp[chiave] <= 20 and temp[chiave]>= 10 ):
-                requests.get(SET_RELAY_1)
-            elif(temp[chiave] >= 40): requests.get(SET_RELAY_2)
-        time.sleep(2)
-        #yield "ciao"
+    while(RUN):
+        temp = requests.get(GET_TEMP).json()
+        stato_r = requests.get(STATUS_RELAY).json()
+        if (temp['T1'] >= 40.0 and stato_r["Relay1"] == 0 ): 
+            requests.get(SET_RELAY_1)
+            print(temp['T1'])
+        elif(temp['T2'] >= 25.0 and True ): 
+            requests.get(SET_RELAY_2)
+            print(temp['T2'])
+
+        print(stato_r)
+        time.sleep(10)
 
 
--- executor.py
+++ executor.py
@@ -3,6 +3,7 @@ from flask import Flask,jsonify
 import requests
 import time
 import esecuzione
+import threading
 
 GET_TEMP = "http://127.0.0.1:5000/temp"
 SET_RELAY_1 = "http://127.0.0.1:5001/set_relay/1"
@@ -19,9 +20,16 @@ def helloworld():
 def get_data():
     response_temps = requests.get(GET_TEMP)
     temp = response_temps.json()
+    esecuzione.RUN = True
     #print(response_temps.json()["T1"]
-    esecuzione.controllo_temp()
-    #return (requests.get(STATUS_RELAY).json())
+    x = threading.Thread(target=esecuzione.controllo_temp, args =())
+    x.start()
+    return (requests.get(STATUS_RELAY).json())
+
+@app.route('/stop')
+def stop_polling():
+    esecuzione.RUN = False
+    return "stopped polling"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True,port=5004)
