725cceb247d962601f8b5c3b42ebad2803f4bfb9
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-08-23 17:51:36 +0200

    aggiunto tutto in un unico file

--- executor.py
+++ executor.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask,jsonify
 import requests
 import time
-import esecuzione
+
 import threading
 from os import getenv
 from requests.auth import HTTPBasicAuth
@@ -15,6 +15,30 @@ GET_TEMP = getenv("GET_TEMP") or "http://127.0.0.1:5000/temp"
 SET_RELAY_1 =  getenv("SET_RELAY1") or "http://127.0.0.1:5001/set_relay/1"
 SET_RELAY_2 = getenv("SET_RELAY2") or "http://127.0.0.1:5001/set_relay/2"
 STATUS_RELAY = getenv("STATUS_RELAY") or "http://127.0.0.1:5001/status"
+
+
+stop_event = threading.Event()  #variabile evento per lo stop 
+
+    
+def controllo_temp(interval):
+    count = 0
+    while(not stop_event.is_set()):
+        count +=1
+        temp = requests.get(GET_TEMP,auth=HTTPBasicAuth(username,password)).json()    #estrae dizionario di temperature
+        stato_r = requests.get(STATUS_RELAY, auth=HTTPBasicAuth(username,password)).json() #estrae dizionare di relays
+        if (temp['T1'] >= 40.0 and stato_r["Relay1"] == 0 ): 
+            requests.get(SET_RELAY_1,auth=HTTPBasicAuth(username,password))
+            print(temp['T1'])
+        elif(temp['T2'] >= 25.0 and True ):
+        #elif(temp['T2'] >= 25.0 and stato_r["Relay2"] == 0 ): 
+            requests.get(SET_RELAY_2,auth=HTTPBasicAuth(username,password))
+            print(temp['T2'])
+
+        print(stato_r)
+        
+        time.sleep(interval)
+    print("thread stopped; count is", count)
+
 
 app = Flask(__name__)
 # auth = HTTPBasicAuth()
@@ -40,7 +64,7 @@ def get_data():
     global x    #per dichiarare di usare la variabile globale
     if x is not None:   #controlla che non è già in esecuzione
         return "thread già in esecuzione"
-    x = threading.Thread(target=esecuzione.controllo_temp, args =(5,), daemon=True) #crea l'oggetto thread per la funzione di polling
+    x = threading.Thread(target=controllo_temp, args =(5,), daemon=True) #crea l'oggetto thread per la funzione di polling
     x.start()
     time.sleep(5)
     return (requests.get(STATUS_RELAY, auth=HTTPBasicAuth(username,password)).json())
@@ -51,9 +75,9 @@ def get_data():
 #@auth.login_required
 def stop_polling():
     global x
-    esecuzione.stop_event.set() #setta l'evento per stoppare il ciclo while
+    stop_event.set() #setta l'evento per stoppare il ciclo while
     x.join()    #esegue la join con il main thread 
-    esecuzione.stop_event.clear()
+    stop_event.clear()
     x = None
     
     return "stopped polling"
