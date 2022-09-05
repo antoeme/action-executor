c1b41b71ac6e311b7e1feeb3be4330f47b4e64f0
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-09-05 11:30:20 +0200

    cambiati nomi env

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ import time
 
 import threading
 from os import getenv
+from dotenv import load_dotenv
+
 from requests.auth import HTTPBasicAuth
 from flask_httpauth import HTTPBasicAuth as log
 from werkzeug.security import generate_password_hash, check_password_hash
 
 username = "daniele" or "antonio"
 password = "Cisco123" or "Dtlab123"
-GET_TEMP = getenv("GET_TEMP") or "http://127.0.0.1:5000/temp"
-SET_RELAY_1 =  getenv("SET_RELAY1") or "http://127.0.0.1:5001/set_relay/1"
-SET_RELAY_2 = getenv("SET_RELAY2") or "http://127.0.0.1:5001/set_relay/2"
-STATUS_RELAY = getenv("STATUS_RELAY") or "http://127.0.0.1:5001/status"
+GET_TEMP = getenv("GET_TEMP") #or "http://127.0.0.1:5000/temp"
+SET_RELAY_1 =  getenv("SET_RELAY1") #or "http://127.0.0.1:5001/set_relay/1"
+SET_RELAY_2 = getenv("SET_RELAY2") #or "http://127.0.0.1:5001/set_relay/2"
+STATUS_RELAY = getenv("STATUS_RELAY") #or "http://127.0.0.1:5001/status"
 
+# load environment variables from '.env' file
+load_dotenv()
 
 stop_event = threading.Event()  #variabile evento per lo stop 
 
@@ -25,7 +29,7 @@ def controllo_temp(interval):
     while(not stop_event.is_set()):
         count +=1
         temp = requests.get(GET_TEMP,auth=HTTPBasicAuth(username,password)).json()    #estrae dizionario di temperature
-        stato_r = requests.get(STATUS_RELAY, auth=HTTPBasicAuth(username,password)).json() #estrae dizionare di relays
+        stato_r = requests.get(STATUS_RELAY, auth=HTTPBasicAuth(username,password)).json() #estrae dizionari di relays
         if (temp['T1'] >= 40.0 and stato_r["Relay1"] == 0 ): 
             requests.get(SET_RELAY_1,auth=HTTPBasicAuth(username,password))
             print(temp['T1'])
